f36f44783dfd8dcdd39e57af9b89c48882b6e0c8
julius bade <julbade21@gmail.com>
2018-08-17 15:23:41 -0700

    finalized some js scrpits

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,16 @@ var beepConverter = function(number) {
 
   if (number < 0) {
     return "Please enter a higher number"
+
   } else if (number > 0 && number % 3 === 0) {
     return "I'm sorry, Dave. I'm afraid I can't do that";
+
   } else if (number.toString().indexOf('1') > -1) {
     return "Boop!!!";
+
   } else if (number % 10 === 0) {
     return "Beep!!!";
+
   } else {
     for(var i = 0; i <= number; i += 1) {
       result.push(i);
@@ -25,7 +29,7 @@ $(document).ready(function() {
 $("form#beep").submit(function(event) {
   event.preventDefault();
 
-  var numberInput = parseInt($("input#enternumber").val());
+  var numberInput = parseInt($("input#enterNumber").val());
   var numberResult = beepConverter(numberInput);
 
   $("#output").text(numberResult);
